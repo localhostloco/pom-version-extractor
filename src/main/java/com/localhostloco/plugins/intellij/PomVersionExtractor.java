7653e02188cfe2c7536e96d2207f1054800a2dfd
Daniel Susumu Ribeiro Yamamoto <daniel.susumu@gmail.com>
2019-09-30 22:16:01 +0200

    adding scaffold

---
+++ src/main/java/com/localhostloco/plugins/intellij/PomVersionExtractor.java
@@ -0,0 +1,12 @@
+package com.localhostloco.plugins.intellij;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class PomVersionExtractor extends AnAction {
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent e) {
+
+  }
+}
